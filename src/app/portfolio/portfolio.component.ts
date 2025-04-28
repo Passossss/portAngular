8c09d7580280e9245a7865045ffc39e8da4d32e8
Gustavo Passos <gusapas26@gmail.com>
2025-04-28 19:28:27 -0300

    .

--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Title } from '@angular/platform-browser';
 import { Project } from '../_models/Project';
-//import { Tag } from '../_models/Tag';
+import { Tag } from '../_models/Tag';
 import { ProjectsService } from '../_services/projects.service';
 
 @Component({
@@ -10,13 +10,55 @@ import { ProjectsService } from '../_services/projects.service';
   styleUrls: ['./portfolio.component.css']
 })
 export class PortfolioComponent implements OnInit {
-  projects: Project[] = [];
+  projects = {} as Project[];
+  isCollapsed: boolean = true;
+  typescript: boolean = false;
+  javascript: boolean = false;
+  python: boolean = false;
+  csharp: boolean = false;
+  java: boolean = false;
+  angular: boolean = false;
+  nodejs: boolean = false;
+  aspnet: boolean = false;
+  react: boolean = false;
 
   constructor(private titleService: Title, private projectService: ProjectsService) {
-    this.titleService.setTitle('Gustavo Passos - Portfolio');
+    this.titleService.setTitle('Gu - Portfolio');
   }
 
   ngOnInit(): void {
     this.projects = this.projectService.GetProjects();
   }
+
+  Filter() {
+    let filterTags: Tag[] = [];
+    if (this.typescript) {
+      filterTags.push(Tag.TYPESCRIPT);
+    }
+    if (this.angular) {
+      filterTags.push(Tag.ANGULAR);
+    }
+    if (this.python) {
+      filterTags.push(Tag.PYTHON);
+    }
+    if (this.csharp) {
+      filterTags.push(Tag.CSHARP);
+    }
+    if (this.java) {
+      filterTags.push(Tag.JAVA);
+    }
+    if (this.nodejs) {
+      filterTags.push(Tag.NODEJS);
+    }
+    if (this.aspnet) {
+      filterTags.push(Tag.ASPNET);
+    }
+    if (this.javascript) {
+      filterTags.push(Tag.JAVASCRIPT);
+    }
+    if (this.react) {
+      filterTags.push(Tag.REACT);
+    }
+    this.projects = this.projectService.GetProjectsByFilter(filterTags);
+}
 }
--- src/app/project-modal/project-modal.component.ts
+++ src/app/project-modal/project-modal.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { BsModalRef } from 'ngx-bootstrap/modal';
+import { CarouselModule } from 'ngx-bootstrap/carousel';
+import { Project } from '../_models/Project';
 //import { ProjectCardComponent } from '../project-card/project-card.component';
 
 @Component({
@@ -7,8 +9,9 @@ import { BsModalRef } from 'ngx-bootstrap/modal';
   templateUrl: './project-modal.component.html',
   styleUrls: ['./project-modal.component.css']
 })
-export class ProjectModalComponent {
 
+export class ProjectModalComponent {
+  project = {} as Project
 
   constructor(public bsModalRef: BsModalRef){
     
